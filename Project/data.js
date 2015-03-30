a494623cbb93e7ea12e7228c2ec9ddd1ac77baa5
qhui <liesel.hui@gmail.com>
2015-03-30 09:46:13 -0400

    ss

--- Project/data.js
+++ Project/data.js
@@ -3,9 +3,9 @@ Country.draw("#countrysvg");
 
 
 var clk = function(id){
-  var width = 400;
-  var height = 400;
-  var radius = Math.min(width,height) / 2;
+  var width = 960,
+      height = 500,
+      radius = Math.min(width, height) / 2;
 
   var color = d3.scale.ordinal()
       .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
@@ -16,33 +16,33 @@ var clk = function(id){
 
   var pie = d3.layout.pie()
       .sort(null)
-      .value(function(d) { return d; });
+      .value(function(d) { return d.population; });
 
-  var svg = d3.select("#info").append("svg")
+  var svg = d3.select("body").append("svg")
       .attr("width", width)
       .attr("height", height)
-      .append("g")
+    .append("g")
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  var vdata = [1, 2, 3, 4, 5, 6, 7];
-      vdata.forEach(function(data2){
+    data1.forEach(function(d) {
+      d[6] = +d[6];
+    });
 
     var g = svg.selectAll(".arc")
-        .data(pie(vdata))
-        .enter().append("g")
+        .data(pie(data1))
+      .enter().append("g")
         .attr("class", "arc");
 
     g.append("path")
         .attr("d", arc)
-            .style("fill", function(d) { return color(d.data2); });
+        .style("fill", function(d) { return color(d.data1[6]); });
 
     g.append("text")
         .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
         .attr("dy", ".35em")
         .style("text-anchor", "middle")
-        .text(function(d) { return d.vdata; });
+        .text(function(d) { return d.data1[2]; });
 
-  });
 
 
 }
