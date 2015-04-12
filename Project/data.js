9bdb2aff2fc432c4770ee63b8cacf489ad85825e
VivianDWD <idoudou92@gmail.com>
2015-04-12 18:43:39 -0400

    f

--- Project/data.js
+++ Project/data.js
@@ -59,6 +59,7 @@ var clk = function(c){
   //--------------------------------------------agency array------------------------------------------------
     var sortedAgencyArray= countryarray.sort(function(a,b){
     return d3.ascending(a[1], b[1]);
+	console.log(sortedAgencyArray);
   });
   
    sortedAgencyArray.forEach(function(d){
