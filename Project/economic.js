9fd295902b1d781813bbaf39d365b7c9ea7b3559
nsalas24 <nsalas2424@gmail.com>
2015-04-20 12:06:59 -0400

    opacity changes

--- Project/economic.js
+++ Project/economic.js
@@ -327,8 +327,8 @@ d3.json("rightdata.json", function(json) {
 				var countries = "#"+firstname;
 		d3.selectAll(countries)
 		//.style("stroke", "black")
-		.style("fill", "yellow")
-		.style("fill-opacity", amount/1000000)/*function(amount){
+		.style("fill", "green")
+		.style("fill-opacity", amount/10000000)/*function(amount){
 		if(amount > 1000000000){return .5;}
 		else {return (amount/950000000);}})*/
 		.on("click",function(){
--- Project/education.js
+++ Project/education.js
@@ -328,7 +328,7 @@ d3.json("rightdata.json", function(json) {
 		d3.selectAll(countries)
 		//.style("stroke", "black")
 		.style("fill", "purple")
-		.style("fill-opacity", amount/1000000)/*function(amount){
+		.style("fill-opacity", amount/10000000)/*function(amount){
 		if(amount > 1000000000){return .5;}
 		else {return (amount/950000000);}})*/
 		.on("click",function(){
