0acaa92b8db4ae7ba76d9bd4e3070c6180032760
VivianDWD <idoudou92@gmail.com>
2015-04-21 13:14:36 -0400

    environment

--- Project/environment.js
+++ Project/environment.js
@@ -346,54 +346,8 @@ var clk = function(c){
 			    	return year[i];//*******************************************************************************************
 			   });
 			//.text(year[i]); //each time creat a piece, add the lable by increasing i?
+		
 			
-//-----------------------------category pie--------------------------------------
-  var w = 240;
-			var h = 240;
-			var outerRadius = w / 2;
-			var innerRadius = 0;
-			var arc = d3.svg.arc()
-							.innerRadius(innerRadius)
-							.outerRadius(outerRadius);
-
-			var pie = d3.layout.pie();
-
-			//Easy colors accessible via a 10-step ordinal scale
-			var color = d3.scale.category20c();
-
-			//Create SVG element
-			var svg = d3.select("#info")
-						.append("svg")
-						.attr("width", w)
-						.attr("height", h);
-
-			//Set up groups
-			var arcs = svg.selectAll("g.arc")
-						  .data(pie(catarray))//*******************************************************************************
-						  .enter()
-						  .append("g")
-						  .attr("class", "arc")
-						  .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
-
-			//d3.selectAll("svg").exit().remove();
-      //Draw arc paths
-			arcs.append("path")
-			    .attr("fill", function(d, i) {
-			    	return color(i);
-			    })
-			    .attr("d", arc);
-
-			//Labels
-
-			arcs.append("text")
-			    .attr("transform", function(d) {
-			    	return "translate(" + arc.centroid(d) + ")";
-			    })
-			    .attr("text-anchor", "middle")
-			   .text(function(d, i) {
-			    	return category[i];//*******************************************************************************************
-			   });
-			//.text(year[i]); //each time creat a piece, add the lable by increasing i?
 
 };
 
