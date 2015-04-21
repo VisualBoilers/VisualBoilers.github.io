15737c73f14c15ab228be5078510377c06907d21
nsalas24 <nsalas2424@gmail.com>
2015-04-21 16:53:04 -0400

    fix health page

--- Project/health.js
+++ Project/health.js
@@ -8,9 +8,7 @@ var clk = function(c){
   var year = [2005,2006,2007,2008,2009,2010,2011,2012,2013,2014];
   var agency=["DOS","DoD","HHS","IAF","MCC","Peace Corps","Treasury","USADF","USAID","USDA"];
   var category=["Democracy, Human Rights, and Governance","Economic Development","Education and Social Services","Environment","Health","Humanitarian Assistance","Multi-Sector","Peace and Security","Program Management"];
-//var sector=["Basic Education","Education and Social Services - General","Higher Education","Social Assistance","Social Policies, Regulations, and Systems","Social Services"];//**change for each one**************************
-
-var sector=["Family Planning and Reproductive Health","Health - General","HIV/AIDS","Malaria","Maternal and Child Health","Nutrition","Other Public Health Threats","Pandemic Influenza and Other Emerging Threats","Tuberculosis","Water Supply and Sanitation"];//**change for each one**************************
+  var sector=["Family Planning and Reproductive Health","Health - General","HIV/AIDS","Malaria","Maternal and Child Health","Nutrition","Other Public Health Threats","Pandemic Influenza and Other Emerging Threats","Tuberculosis","Water Supply and Sanitation"];//**change for each one**************************
 
   data.forEach(function(d){
     if(d[7] == c.id){
@@ -132,14 +130,14 @@ var sector=["Family Planning and Reproductive Health","Health - General","HIV/AI
 
   });
   catarray.push(catamount);
- ///------------------------------------------try sector-----------------------------
+ /*///------------------------------------------try sector-----------------------------*/
  var extractCata=[];//restore********************************************************************************
  extractCata=sortedCategoryArray;
  console.log(extractCata[1]);
   var extractCataNew=[];
    var q=0;
    for(p=0;p<extractCata.length;p++){
-   if(extractCata[p][4]===category[4]){///change for each category
+   if(extractCata[p][4]===category[4]){   /*///change for each category*/
 	   extractCataNew[q]=extractCata[p];
 	   q++;
 	   }
