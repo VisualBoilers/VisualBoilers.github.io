f4bed7788e79f32412d6e65e10c7afa3a32e20ea
VivianDWD <idoudou92@gmail.com>
2015-04-21 13:31:54 -0400

    health

--- Project/health.js
+++ Project/health.js
@@ -8,7 +8,7 @@ var clk = function(c){
   var year = [2005,2006,2007,2008,2009,2010,2011,2012,2013,2014];
   var agency=["DOS","DoD","HHS","IAF","MCC","Peace Corps","Treasury","USADF","USAID","USDA"];
   var category=["Democracy, Human Rights, and Governance","Economic Development","Education and Social Services","Environment","Health","Humanitarian Assistance","Multi-Sector","Peace and Security","Program Management"];
-  var sector=["Family Planning and Reproductive Health","Health - General","HIV/AIDS","Malaria","Maternal and Child Health","Nutrition","Other Public Health Threats","Pandemic Influenza and Other Emerging Threats (PIOET)","Tuberculosis","Water Supply and Sanitation"];//**change for each one**************************
+  var sector=["Family Planning and Reproductive Health","Health - General","HIV/AIDS","Malaria","Maternal and Child Health","Nutrition","Other Public Health Threats","Pandemic Influenza and Other Emerging Threats","Tuberculosis","Water Supply and Sanitation"];//**change for each one**************************
 
   data.forEach(function(d){
     if(d[7] == c.id){
