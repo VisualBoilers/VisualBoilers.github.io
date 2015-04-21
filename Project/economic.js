405145e84cf66bc1c4fad0d9b526dcf68e1a5fbe
VivianDWD <idoudou92@gmail.com>
2015-04-21 00:06:55 -0400

    extract

--- Project/economic.js
+++ Project/economic.js
@@ -126,18 +126,18 @@ var clk = function(c){
   });
   catarray.push(catamount);
   ///------------------------------------------try sector-----------------------------
- extractCata=sortedCategoryArray;
- console.log(extractCata[1]);
-  var extractCataNew=[];
+ //extractCata=sortedCategoryArray;
+ //console.log(extractCata[1]);
+ // var extractCataNew=[];
    var q=0;
    for(p=0;p<extractCata.length;p++){
-   if(extractCata[p][4]=sector[0]){
-	   extractCataNew[q]=extractCata[p];
+   if(sortedCategoryArray[p][4]=sector[0]){
+	   extractCata[q]=sortedCategoryArray[p];
 	   q++;
 	   }
    else{q=q;}
    }
-   console.log(extractCataNew);
+   console.log(extractCata);
   //-------------------------------------------total number--------------------------
   
    var w = 240;
