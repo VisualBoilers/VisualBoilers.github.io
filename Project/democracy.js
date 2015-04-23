3300ab87aa5b606a8727c8ab709636112b68b739
VivianDWD <idoudou92@gmail.com>
2015-04-22 21:03:52 -0400

    iz

--- Project/democracy.js
+++ Project/democracy.js
@@ -247,7 +247,8 @@ var clk = function(c){
 				.text(function(i){
 					
 					console.log(z);
-					return sector[z];
+					return sector[z]+"amount is "+sectorarray[z];
+					
 					
 				       					});
 				})
